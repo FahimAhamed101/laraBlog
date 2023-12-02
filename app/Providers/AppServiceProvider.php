378dca56488783223ed96e4e28bfb68b20a389c8
fahim <fahimahamedweb@gmail.com>
2023-12-03 00:46:43 +0600

    added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
+    {   URL::forceScheme('https');
         Paginator::useBootstrap();
         if( Schema::hasTable('categories') ) {
             
