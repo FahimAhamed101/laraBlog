6205bd973f4d2ee63f345e1d2d659bec76f4a2db
fahim <fahimahamedweb@gmail.com>
2023-12-03 22:54:56 +0600

    adeed

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*','https://lara-blog-opal.vercel.app', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
