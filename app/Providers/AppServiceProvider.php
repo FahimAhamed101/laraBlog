37bc8162ffd466b34169ab15676c9ec65a17a3b3
fahim <fahimahamedweb@gmail.com>
2024-02-02 03:09:08 +0600

    vercel deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {   
        
-        \URL::forceScheme('https');
+        
+        
         Paginator::useBootstrap();
         if( Schema::hasTable('categories') ) {
             
